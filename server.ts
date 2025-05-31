bb2826ca9ff2c2efbb9147c63b573aad78b023c4
Nwogu Michael Daberechi <codesmiles@Nwogus-MacBook-Pro.local>
2025-05-31 13:54:00 +0100

    updae

--- server.ts
+++ server.ts
@@ -2,12 +2,9 @@ import dotenv from "dotenv";
 dotenv.config();
 import mongoose from "mongoose";
 
-import { mongoConfig, app, NODE_ENV } from "./src";
+import { mongoConfig, app } from "./src";
 
-//  trying something on multithreading
- import cluster from "node:cluster";
- import { availableParallelism as numCPUs } from "node:os";
- import process from "node:process";
+
 
 
 
@@ -29,29 +26,38 @@ const start = () => {
     })
 }
 
-if (process.env.NODE_ENV === NODE_ENV.DEVELOPMENT) { 
-    start();
-}
-else {
-    // Placeholder statement
-    console.log("Worker process started");
-    //  check if the current process is the master process
-     if (cluster.isPrimary) {
-         console.log(`Primary ${process.pid} is running`);
+start();
+
+//  trying something on multithreading
+//  import cluster from "node:cluster";
+//  import { availableParallelism as numCPUs } from "node:os";
+//  import process from "node:process";
+
+
+// if(process.env.NODE_ENV === NODE_ENV.DEVELOPMENT) {
+//     console.log("Development mode is ON");
+//     start();
+// }
+// else {
+//     // Placeholder statement
+//     console.log("Worker process started");
+//     //  check if the current process is the master process
+//      if (cluster.isPrimary) {
+//          console.log(`Primary ${process.pid} is running`);
         
-        //   Fork workers
-         for (let i = 0; i < numCPUs(); i++) {
-             cluster.fork();
-         }
+//         //   Fork workers
+//          for (let i = 0; i < numCPUs(); i++) {
+//              cluster.fork();
+//          }
         
-         cluster.on('exit', (worker) => { 
-               console.log(`Worker ${worker.process.pid} died. Restarting...`);
-             cluster.fork();
-         })
-     }
-     else{
-         start();
-         console.log(`Worker ${process.pid} started`);
-     }
-}
+//          cluster.on('exit', (worker) => { 
+//                console.log(`Worker ${worker.process.pid} died. Restarting...`);
+//              cluster.fork();
+//          })
+//      }
+//      else{
+//          start();
+//          console.log(`Worker ${process.pid} started`);
+//      }
+// }
 
